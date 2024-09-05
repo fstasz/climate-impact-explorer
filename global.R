412c75693a10e9919da20dbc98bf1f1a9888f4aa
Frank Staszak <fs4364@gmail.com>
2024-09-05 16:25:28 -0500

    Formatting and update README.

--- global.R
+++ global.R
@@ -1,12 +1,11 @@
-# global.R
+# Load the static text config.
+source("textConfig.R")
 
+# global.R
 # install.packages(c("shiny", "leaflet", "dplyr"))
 library(shiny)
 library(leaflet)
 library(dplyr)
-
-# Load the static text config.
-source("textConfig.R")
 
 # Load the temperature data.
 data <- read.csv(textContent$global$filePath)
